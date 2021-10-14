aeba9908e08b06af42507761f47f6c56309e0b15
Liron Saedi <Liron.Saedi@GMR.local>
2021-10-13 18:18:49 -0700

    Halfway done iwth all of the functions

---
+++ Graph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unweighted_and_Undirected_Graphs
+{
+    class Graph<T>
+    {
+        public List<Vertex<T>> Vertices { get; private set; }
+
+        public int VertexCount => Vertices.Count;
+
+        public Graph()
+        {
+            Vertices = new List<Vertex<T>>();
+        }
+
+        //.neightbors.add
+        void AddVertex(Vertex<T> vertex)
+        {
+            if (vertex == null)
+            {
+                throw new ArgumentNullException(nameof(vertex));
+            }
+            else if (vertex.Neighbors.Count != 0 || Vertices.Contains(vertex))
+            {
+                throw new
+            }
+        }
+    }
+}
---
+++ Vertex.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unweighted_and_Undirected_Graphs
+{
+    class Vertex
+    {
+    }
+}
